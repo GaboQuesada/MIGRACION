ff21ffad2c90f6daa50da150257e82409a8dd51a
Gabriel Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2023-10-16 16:04:22 -0600

    Update account_move_reversal.py

--- account_move_reversal.py
+++ account_move_reversal.py
@@ -1,13 +1,14 @@
-def _prepare_default_reversal(self, move):
+ def _prepare_default_reversal(self, move):
         reverse_date = self.date if self.date_mode == 'custom' else move.date
         return {
-            'ref': _('Reversal of: %(move_name)s, %(reason)s', move_name=move.name, reason=self.reason) 
+            'ref': _('Reversal of: %(move_name)s, %(reason)s', move_name=move.name, reason=self.reason)
                    if self.reason
                    else _('Reversal of: %s', move.name),
             'date': reverse_date,
+            'invoice_date_due': reverse_date,
             'invoice_date': move.is_invoice(include_receipts=True) and (self.date or move.date) or False,
             'journal_id': self.journal_id.id,
             'invoice_payment_term_id': None,
             'invoice_user_id': move.invoice_user_id.id,
-            'auto_post': True if reverse_date > fields.Date.context_today(self) else False,
+            'auto_post': 'at_date' if reverse_date > fields.Date.context_today(self) else 'no',
         }
